8e7bd571c3cb42299019b45f1adebfbdd0fde2e9
Johansen512 <tekstogtext@gmail.com>
2021-02-09 21:37:27 +0100

    barebone form

--- src/Components/Loginform.js
+++ src/Components/Loginform.js
@@ -2,15 +2,43 @@
 /**@jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { dataContext } from "../Contexts/DataContext";
-import {useContext} from "react";
+import {useContext, useState} from "react";
+import { useForm } from "react-hook-form";
 
 
 
 const Loginform = () => {
+
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = data => {console.log (data.email, data.password)}
+
+
+    
+
+
     return (  
 
-        <div><h1>Dobbelt Piv</h1></div>
+        <section>
+        <form  onSubmit={handleSubmit(onSubmit)}>
+        
+        <label for="number">Indtast e-mail og password</label>
+          
+         
+         
+          <input type='e-mail' name="email"  ref={register({ required: true })} />
+          <input type='password' name="password"  ref={register({ required: true })} />
+        
+          
+          
+          
+          {errors.exampleRequired && <span>This field is required</span>}
+          
+          <input type="submit" />
+        </form>
+    
+    </section>
     );
 }
  
-export default Loginform;
+export default Loginform
